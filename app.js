bf96c58dd71712c81c2036ea907a4f01fe80de24
Hien Nguyen <99568099+hienthaonguyen@users.noreply.github.com>
2023-01-02 13:03:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from "./components/navbar"
+
+function App(){
+    return (<React.Fragment>
+        <Navbar/>
+    </React.Fragment>);
+}
+
+export default App;
